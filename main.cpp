efd8bf060dd9cfcf3c89a5c6381b53b47aa9467a
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-30 22:26:19 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -7,8 +7,26 @@ using namespace std;
 class Solution {
  public:
   vector<int> getRow(int rowIndex) {
-    // FIXME
-    return {};
+    vector<int> previous_row;
+    vector<int> row;
+    if (0 == rowIndex) {
+      row = {1};
+    } else if (1 == rowIndex) {
+      row = {1,1};
+    } else if (1 < rowIndex) {
+      row = {1,1};
+      for (int i = 2; i <= rowIndex; ++i) {
+        previous_row = move(row);
+        size_t length = previous_row.size() + 1;
+        row.resize(length);
+        row.front() = row.back() = 1;
+        for (int k = 1; k < length - 1; ++k) {
+          row[k] = previous_row[k-1] + previous_row[k];
+        }
+      }
+    }
+
+    return row;
   }
 };
 
@@ -17,7 +35,6 @@ void TestGetRow() {
   assert((vector<int>{1}) == s.getRow(0));
   assert((vector<int>{1,1}) == s.getRow(1));
   assert((vector<int>{1,3,3,1}) == s.getRow(3));
-
 }
 
 int main() {
